41e0b72044dde459512c138e3a5f597ad26356cd
Adyie9 <114578740+Adyie9@users.noreply.github.com>
2024-04-01 20:19:40 +0530

    Update quotes.js

--- ipj/quotes.js
+++ ipj/quotes.js
@@ -36,12 +36,9 @@ let quotes = [
   'The art of programming is the skill of controlling complexity. - Unknown'
 ];
 
-// let btn=document.querySelector('.btn');
-// btn.addEventListener('click', displayQuote);
 
 function displayQuote() {
   //create an index of a random number 
-  //convert it into between 0 to length of quotes[]
   let index = Math.floor(Math.random() * quotes.length);
 
   //display the quote of that index
